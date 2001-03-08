ae9faa37122014880dbe839509eeb097fda1c833
Ben Collins <bcollins@debian.org>
2001-03-08 15:34:30 +0000

    Make sure the policy files end in .pol

--- debsig-verify.c
+++ debsig-verify.c
@@ -274,8 +274,9 @@ int main(int argc, char *argv[]) {
 	ds_printf(DS_LEV_ALWAYS, "  Policies in: %s", buf);
 
     while ((pd_ent = readdir(pd)) != NULL && (pol == NULL || list_only)) {
+	char *ext = strstr(pd_ent->d_name, ".pol");
 	/* Make sure we have the right name format */
-	if (strstr(pd_ent->d_name, ".pol") == NULL)
+	if (ext == NULL || (ext - pd_ent->d_name) + 4 != strlen(pd_ent->d_name))
 	    continue;
 
 	if (force_file != NULL && strcmp(pd_ent->d_name, force_file))

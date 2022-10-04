guillemj/debsig-verify

commit 29f80390bebba1deb304ac58e0acc0644cb130eb

openpgp-gpg: When matching on getKeyID match on fingerprint
If we are asked to match on a policy ID, we should check whether it
matches a fingerprint (or keyID) when we find one in the keyring. And
only if not, then match on a UID.

This has meant that the selection and verification policy ID criteria
based on a fingerprint or keyID was not effective until now. This does
not seem like a serious issue though, as the policy needed to be found
based on the fingerprint/keyID of the signature, the origin and type
checks were in place and working, and the signature needed to verify
successfully based on the specific policy keyring.

src/openpgp-gpg.c
@@ -195,6 +195,8 @@ gpg_getKeyID(const char *originID, const struct match *mtc)
     }
 
     while ((nread = getline(&buf, &buflen, ds)) >= 0) {
+        char *fpr;
+
 	if (state == KEYID_UNKNOWN) {
             if (!match_prefix(buf, "pub:"))
 		continue;
@@ -204,7 +206,11 @@ gpg_getKeyID(const char *originID, const struct match *mtc)
         } else if (state == KEYID_PUB) {
             if (!match_prefix(buf, "fpr:"))
 		continue;
-            ret = get_colon_field(buf, COLON_FIELD_FPR_ID);
+            fpr = get_colon_field(buf, COLON_FIELD_FPR_ID);
+            if (eqKeyID(fpr, mtc->id)) {
+                ret = fpr;
+                break;
+            }
             state = KEYID_FPR;
         } else if (state == KEYID_FPR) {
             char *uid;
@@ -222,6 +228,7 @@ gpg_getKeyID(const char *originID, const struct match *mtc)
             free(uid);
 
             /* Fingerprint match found. */
+            ret = fpr;
             break;
         }
     }

commit c9eaae4c35d334871dc733deb7cd6e61b484bc31

openpgp-gpg: Move and initialize fpr declaration outside of the getline loop
The loop uses this variable on subsequent lines when matched to return
the fingerprint found, so we need to widen its scope. In addition we
need to initialize it to NULL, even though the code takes care of not
using ret when it is NULL, and only set ret to fpr, on the specific
state.

Fixes: commit 29f80390bebba1deb304ac58e0acc0644cb130eb
Warned-by: gcc
Changelog: silent

src/openpgp-gpg.c
@@ -178,6 +178,7 @@ gpg_getKeyID(const char *keyring, const char *match_id)
     int pipefd[2];
     FILE *ds;
     char *ret = NULL;
+    char *fpr = NULL;
     enum keyid_state state = KEYID_UNKNOWN;
 
     if (match_id == NULL)
@@ -208,8 +209,6 @@ gpg_getKeyID(const char *keyring, const char *match_id)
     }
 
     while ((nread = getline(&buf, &buflen, ds)) >= 0) {
-        char *fpr;
-
         if (buf[nread - 1] != '\n') {
           ds_printf(DS_LEV_DEBUG, "        getKeyID: found truncated input from GnuPG, aborting");
           break;
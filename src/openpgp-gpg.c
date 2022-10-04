guillemj/debsig-verify

commit 14e70de6df5c8ee3d9e11f9556dac83593de8639

openpgp-gpg: Switch from internal gpg --list-packets to --verify --status-fd
The GnuPG --list-packets command is for debugging and its interface is
not intended to be used by other tools.

Switch to use --verify with --status-fd, which is a supported interface
with a defined output format. This also simplifies the code parsing.

While this is not an ideal interface, and a dedicated one to inspect a
signature would be better, this is the best we can get right now.

Ref: #1001331

src/openpgp-gpg.c
@@ -26,6 +26,7 @@
 #include <sys/wait.h>
 
 #include <ctype.h>
+#include <fcntl.h>
 #include <string.h>
 #include <stdbool.h>
 #include <stdlib.h>
@@ -118,6 +119,12 @@ enum colon_fields {
     COLON_FIELD_UID_ID = 10,
 };
 
+enum errsig_fields {
+    ERRSIG_FIELD_SELF = 2,
+    ERRSIG_FIELD_KEY_ID = 3,
+    ERRSIG_FIELD_FPR_ID = 9,
+};
+
 static bool
 match_prefix(const char *str, const char *prefix)
 {
@@ -144,6 +151,12 @@ get_field(const char *str, int field_sep, int field_num)
         return NULL;
 
     return strndup(str, end - str);
+}
+
+static char *
+get_space_field(const char *str, int field_num)
+{
+    return get_field(str, ' ', field_num);
 }
 
 static char *
@@ -289,6 +302,7 @@ gpg_getSigKeyID(struct dpkg_ar *deb, const char *type)
     pid = subproc_fork();
     if (pid == 0) {
         struct command cmd;
+        int null_fd;
 
 	/* Here we go */
 	m_dup2(pread[1], 1);
@@ -298,8 +312,13 @@ gpg_getSigKeyID(struct dpkg_ar *deb, const char *type)
 	close(pwrite[0]);
 	close(pwrite[1]);
 
+	null_fd = open("/dev/null", O_WRONLY);
+	m_dup2(null_fd, STDERR_FILENO);
+
 	command_gpg_init(&cmd);
-	command_add_args(&cmd, "--list-packets", "-q", "-", NULL);
+	command_add_args(&cmd, "--keyring", "/dev/null", NULL);
+	command_add_args(&cmd, "--status-fd", "1", NULL);
+	command_add_args(&cmd, "--verify", "-", "/dev/null", NULL);
 	command_exec(&cmd);
     }
     close(pread[1]); close(pwrite[0]);
@@ -314,8 +333,6 @@ gpg_getSigKeyID(struct dpkg_ar *deb, const char *type)
 
     /* Now, let's see what gpg has to say about all this */
     while ((nread = getline(&buf, &buflen, ds_read)) >= 0) {
-        char *d;
-
         if (buf[nread - 1] != '\n') {
           ds_printf(DS_LEV_DEBUG, "        getKeyID: found truncated input from GnuPG, aborting");
           break;
@@ -326,43 +343,24 @@ gpg_getSigKeyID(struct dpkg_ar *deb, const char *type)
         if (buf[0] == '#')
             continue;
 
-        if (state == KEYID_UNKNOWN) {
-            if (match_prefix(buf, ":signature packet:")) {
-                const char keyid_str[] = "keyid";
+        if (strncmp(buf, "[GNUPG:]", 8) != 0)
+            continue;
 
-                if ((c = strchr(buf, '\n')) != NULL)
-                    *c = '\0';
-
-                /* If we find a KeyID, save it in case we cannot find an
-                 * Issuer Fingerprint. */
-                d = strstr(buf, "keyid");
-                if (d) {
-                    d += strlen(keyid_str);
-                    while (isspace(*d))
-                        d++;
-                    ret = d;
-                }
-            }
-            /* Signature packet found. */
-            state = KEYID_SIG;
-        } else if (state == KEYID_SIG) {
-            const char issuer_fpr_str[] = "issuer fpr v";
-
-            d = strstr(buf, issuer_fpr_str);
-            if (d == 0)
-                continue;
-            if ((c = strchr(buf, '\n')) != NULL)
-                *c = '\0';
-            d += strlen(issuer_fpr_str);
-            while (isdigit(*d))
-                d++;
-            while (isspace(*d))
-                d++;
-            ret = d;
-            ret[OPENPGP_FPR_LEN] = '\0';
-            /* Issuer Fingerprint match found. */
-            break;
+        ret = get_space_field(buf, ERRSIG_FIELD_SELF);
+        if (strcmp(ret, "ERRSIG") != 0) {
+            free(ret);
+            ret = NULL;
+            continue;
         }
+
+        free(ret);
+        ret = get_space_field(buf, ERRSIG_FIELD_FPR_ID);
+        if (strcmp(ret, "-") != 0)
+            break;
+
+        free(ret);
+        ret = get_space_field(buf, ERRSIG_FIELD_KEY_ID);
+        break;
     }
     if (ferror(ds_read))
 	ohshit("error reading from gpg");
@@ -376,7 +374,9 @@ gpg_getSigKeyID(struct dpkg_ar *deb, const char *type)
     else
 	ds_printf(DS_LEV_DEBUG, "        getSigKeyID: got %s for %s key", ret, type);
 
-    return strdup(ret);
+    if (ret)
+      return strdup(ret);
+    return NULL;
 }
 
 static int

commit c9b7e69ab601c5a2375e5073da21a71e9ad698ea

openpgp-gpg: Remove unused variables
Fixes: commit 14e70de6df5c8ee3d9e11f9556dac83593de8639
Warned-by: gcc
Changelog: silent

src/openpgp-gpg.c
@@ -276,8 +276,7 @@ gpg_getSigKeyID(struct dpkg_ar *deb, const char *name)
     off_t len = checkSigExist(deb, name);
     pid_t pid;
     FILE *ds_read;
-    char *c, *ret = NULL;
-    enum keyid_state state = KEYID_UNKNOWN;
+    char *ret = NULL;
 
     if (!len)
 	return NULL;